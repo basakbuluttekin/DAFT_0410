4213555fb76a33617713af197c4067d42b0dc853
Basak Buluttekin <muhlisebuluttekin@gmail.com>
2023-05-09 18:00:09 +0200

    answers

---
+++ module_2/Lab_24_Data-Visualization-with-Tableau/Lab_24.ts
@@ -0,0 +1,4 @@
+Lab_24
+https://public.tableau.com/views/Lab_24_16836464498470/NFLTeamPerformanceDashboard?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link
+
+https://public.tableau.com/views/Lab_24_2_16836478558910/Dashboard1?:language=en-GB&publish=yes&:display_count=n&:origin=viz_share_link
